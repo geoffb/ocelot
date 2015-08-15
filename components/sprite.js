532d46185d31d42d26c0db0520f6ba3c201357ee
Geoff Blair <gmblair@gmail.com>
2015-08-15 12:03:51 -0700

    Spritesheet positioning for sprite component

--- components/sprite.js
+++ components/sprite.js
@@ -2,9 +2,14 @@ var assets = require("../assets");
 
 exports.render = function (ctx) {
 	var sprite = this.sprite;
-	var image = assets.get(sprite.path);
-	var x = -Math.round(image.width / 2);
-	var y = -Math.round(image.height / 2);
-	ctx.globalAlpha = sprite.alpha;
-	ctx.drawImage(image, x, y);
+	var size = sprite.size;
+	var image = assets.get(sprite.image);
+	var width = Math.floor(image.width / size);
+	var sx = sprite.index % width;
+	var sy = Math.floor(sprite.index / width);
+	ctx.drawImage(
+		image,
+		sx * size, sy * size, size, size,
+		-size / 2, -size / 2, size, size
+	);
 };
